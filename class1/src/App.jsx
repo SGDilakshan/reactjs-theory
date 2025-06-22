9a4383c50f9c0a19e5836522983459d197ad2646
SGDilakshan <sgd.dilakshan@gmail.com>
2025-06-23 02:28:35 +0530

    Update first day works

---
+++ class1/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+import { LearnComponents } from "./components/LearnComponents"
+
+function App() {
+    return (
+      <Fragment>
+        <div>Dilakshan React Tutorial</div>
+        <LearnComponents/>
+      </Fragment>
+    ); 
+}
+
+export default App
